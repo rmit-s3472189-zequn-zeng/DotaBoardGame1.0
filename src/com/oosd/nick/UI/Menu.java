8a56ce8deba5b53db2bf10603230872d86003739
rmit-s3472189-zequn-zeng <s3472189@student.rmit.edu.au>
2016-03-17 22:01:19 +1100

    /*implement function of menu partly. lack of menu action bu zequn 17.03.2016 10:01*/

--- src/com/oosd/nick/UI/Menu.java
+++ src/com/oosd/nick/UI/Menu.java
@@ -11,33 +11,23 @@ import com.oosd.nick.game.heroInfo;
 public class Menu extends Component{
 
 	private heroInfo hero_info;
-	int amount = 5;
 	int  locx,locy; //location of menue;
+	Point p;
 	public Menu (Graphics g, Point loc){
+		p=loc;
 		locx=loc.x;
 		locy=loc.y;
 		
 	}
 	
-	public  void setamount(int amount){
-		this.amount=amount;
-	}
-	
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.RED);
-		for (int i=0;i<amount;i++){
-			g.drawRect(locx, locy+i*50, 50, 50);
-		}
-		for(int i =1;i<=amount;i++){
-			g.setColor(Color.DARK_GRAY);
-			g.drawString("Test", locx+10, locy+50*i-25);
-		}
+		new MenuItem(g,p);
 	}
 
 	@Override
 	public boolean onMouseClicked(Point p) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 }
--- src/com/oosd/nick/UI/MenuItem.java
+++ src/com/oosd/nick/UI/MenuItem.java
@@ -1,12 +1,29 @@
 package com.oosd.nick.UI;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class MenuItem extends Menu{
 
+	
+	private String item []={" ","Moving","Attack"};
+	public int len=item.length;
 	public MenuItem(Graphics g, Point loc) {
 		super(g, loc);
-		// TODO Auto-generated constructor stub
+		drawcell(g);
+		addItem(g);
 	}
-
+	
+	public void drawcell(Graphics g){
+		for (int i=0;i<len-1;i++){
+			g.drawRect(locx, locy+i*50, 50, 50);
+		}
+	}
+	
+	public void addItem(Graphics g){
+		for(int i =0;i<item.length;i++){
+			g.setColor(Color.DARK_GRAY);
+			g.drawString(item[i], locx+10, locy+50*i-25);
+		}
+	}
 }
